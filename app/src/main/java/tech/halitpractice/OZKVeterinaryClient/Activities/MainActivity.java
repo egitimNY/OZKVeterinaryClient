258a0fbc443d9738afc23f0f4ab19f8de161092e
Halit <egitimtraining@gmail.com>
2021-12-26 23:56:56 +0300

    Bottom Navigation Bar ( Home Page - Call Veterinary - Exit Button )

--- app/src/main/java/tech/halitpractice/OZKVeterinaryClient/Activities/MainActivity.java
+++ app/src/main/java/tech/halitpractice/OZKVeterinaryClient/Activities/MainActivity.java
@@ -2,8 +2,11 @@ package tech.halitpractice.OZKVeterinaryClient.Activities;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -16,6 +19,8 @@ public class MainActivity extends AppCompatActivity {
 
     private SharedPreferences getSharedPreferences;
     private GetSharedPreferences preferences;
+    private ImageView cikisYap,aramaYapButton,anasayfaButton;
+    private ChangeFragments changeFragments;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,17 +30,48 @@ public class MainActivity extends AppCompatActivity {
         getFragment();
         tanimla();
         kontrol();
+        action();
 
     }
 
     private void getFragment() {
-        ChangeFragments changeFragments = new ChangeFragments(MainActivity.this);
+        changeFragments = new ChangeFragments(MainActivity.this);
         changeFragments.change(new HomeFragment());
+    }
+
+    public void action(){
+        anasayfaButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changeFragments.change(new HomeFragment());
+            }
+        });
+        cikisYap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                GetSharedPreferences getSharedPreferences = new GetSharedPreferences(MainActivity.this);
+                getSharedPreferences.deleteToSession();
+                Intent intent = new Intent(MainActivity.this,MainActivity.class);
+                startActivity(intent);
+
+            }
+        });
+        aramaYapButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("tel:000000"));
+                startActivity(intent);
+            }
+        });
     }
 
     private void tanimla() {
         preferences = new GetSharedPreferences(MainActivity.this);
         getSharedPreferences = preferences.getSession();
+        anasayfaButton = findViewById(R.id.anasayfaButton);
+        cikisYap = findViewById(R.id.cikisYap);
+        aramaYapButton = findViewById(R.id.aramaYapButton);
 
     }
 
